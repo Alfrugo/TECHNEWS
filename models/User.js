8d53cb591ac9bbdad96ada60b268778b47586614
Alfrugo <adiez@mtm-inc.net>
2022-02-05 15:40:12 -0600

    added password hasshing

--- models/User.js
+++ models/User.js
@@ -1,6 +1,9 @@
 const{ Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection')
 
+const bcrypt = require('bcrypt');  // https://www.npmjs.com/package/bcrypt 
+
+
 //create our User model
 
 class User extends Model {}
@@ -42,9 +45,16 @@ User.init(
         }
         
     },
+    
     {
         // TABLE CONFIGURATION OPIONS  GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration)
-
+        hooks: {
+            // set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData) {        // async/await, works in tandem to make this async function look more like a regular synchronous function expression
+                newUserData.password = await bcrypt.hash(newUserData.password, 10)
+                return newUserData;
+            }
+        },
         // pass in our imported sequelize connection (the direct connection to our database)
         sequelize,
         // don't automatically create createdAt/updatedAt timestamp fields
