839891902dbf7f6c82014f9c872098d7216577f3
Alfrugo <adiez@mtm-inc.net>
2022-02-10 17:30:39 -0600

    started adding handlebars

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+const path = require('path');
+
+const exphb = require('express-handlebars');
+const hbs = exphb.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handelbars');
+
+
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));   // the public folder is served as static content
 
 // turn on routes
 app.use(routes);
